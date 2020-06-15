08e4bd3f9ce2fa1e205ea394e35f4a753765e8c1
Francis Tseng <f+accounts@frnsys.com>
2020-06-15 14:38:34 -0400

    tweak gifs

--- make_gifs.sh
+++ make_gifs.sh
@@ -5,13 +5,24 @@ for dir in cartograms/hex/**; do
     for f in ${dir}/*.png; do
         # `mogrify` is supposed to do this in place but
         # would not, strangely. So use `convert` instead
-        convert "$f" -gravity SouthEast -crop 850x500+0+0 +repage "$f"
+        convert "$f" -gravity SouthEast -crop 825x550+0+0 +repage "$f"
     done
 done
+
+# Crop real versions
+for dir in cartograms/real/**; do
+    for f in ${dir}/*.png; do
+        # `mogrify` is supposed to do this in place but
+        # would not, strangely. So use `convert` instead
+        convert "$f" -gravity SouthEast -crop 950x700+0+0 +repage "$f"
+    done
+done
+
 
 for dir in cartograms/**/**; do
     OUT=${dir/cartograms\//}
     OUT=${OUT/\//_}.gif
     echo $OUT
-    convert -delay 10 -loop 0 ${dir}/*.png gifs/$OUT
+    LAST=$(ls ${dir}/*.png | tail -n 1)
+    convert -delay 50 -loop 0 ${dir}/*.png -delay 200 $LAST gifs/$OUT
 done
